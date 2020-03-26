1de2c3f2d451f977b977b52df536096425d2003c
Diego Pimentel <diego.pimentel@hotmail.com>
2020-03-26 14:52:17 -0300

    Front-end da Aplicação

--- backend/src/controllers/IncidentController.js
+++ backend/src/controllers/IncidentController.js
@@ -26,7 +26,7 @@ module.exports = {
     },
 
     async create(request, response) {
-        const { title, description, value } = resquest.body;
+        const { title, description, value } = request.body;
         const ong_id = request.headers.authorization;
 
         const [id] = await connection('incidents').insert({
