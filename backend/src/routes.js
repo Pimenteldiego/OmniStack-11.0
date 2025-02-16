05167c1afbb6597492e0d454050a53d5e1a0daf4
Pimentel <59776126+Pimenteldiego@users.noreply.github.com>
2025-02-15 21:17:41 -0300

    Update routes.js

--- backend/src/routes.js
+++ backend/src/routes.js
@@ -9,9 +9,7 @@ const SessionController = require('./controllers/SessionController');
 
 const routes = express.Router();
 
-routes.post('/sessions', SessionController.create);
-
-const getOngsLimiter = rateLimit({
+const createSessionLimiter = rateLimit({
     windowMs: 15 * 60 * 1000, // 15 minutes
     max: 100, // limit each IP to 100 requests per windowMs
 });
