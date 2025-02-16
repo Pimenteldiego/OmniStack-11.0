f779d67fed4a5d27fdb231f09ecd977b94f7991b
Pimentel <59776126+Pimenteldiego@users.noreply.github.com>
2025-02-15 21:13:51 -0300

    Update routes.js

--- backend/src/routes.js
+++ backend/src/routes.js
@@ -15,6 +15,7 @@ const getOngsLimiter = rateLimit({
     windowMs: 15 * 60 * 1000, // 15 minutes
     max: 100, // limit each IP to 100 requests per windowMs
 });
+routes.post('/sessions', createSessionLimiter, SessionController.create);
 
 routes.get('/ongs', getOngsLimiter, OngController.index);
 
